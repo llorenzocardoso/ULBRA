d36046510a4a35ac5bf60af3c8206d1cd234111a
Lorenzo Cardoso <102180476+llorenzocardoso@users.noreply.github.com>
2023-08-23 19:35:15 -0300

    update 2023-08-23

---
+++ ulbra_2023_2/fundamentos_inteligencia_artificial/aula03/ex2.js
@@ -0,0 +1,57 @@
+function limite_rapido(x) {
+    if (x > 0) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+function funcao_rampa(x) {
+    if (x < 0) {
+        return 0;
+    } else if (x >= 0 && x <= 1) {
+        return x;
+    } else {
+        return 1;
+    }
+}
+
+function funcao_sigmoide(x) {
+    if (x >= 0) {
+        return 1 - 1 / (1 + Math.exp(x));
+    } else {
+        return -1 + 1 / (1 - Math.exp(x));
+    }
+}
+
+function main() {
+    const entrada1 = parseFloat(prompt("Digite a primeira entrada: "));
+    const entrada2 = parseFloat(prompt("Digite a segunda entrada: "));
+    const entrada3 = parseFloat(prompt("Digite a terceira entrada: "));
+    const peso1 = parseFloat(prompt("Digite o peso 1: "));
+    const peso2 = parseFloat(prompt("Digite o peso 2: "));
+    const peso3 = parseFloat(prompt("Digite o peso 3: "));
+    
+    const opc = parseInt(prompt(`
+MENU:
+1 - Limite rápido
+2 - Função rampa
+3 - Função Sigmoide
+Digite a função desejada: `));
+    
+    let resultado;
+    if (opc === 1) {
+        resultado = limite_rapido(entrada1 * peso1 + entrada2 * peso2 + entrada3 * peso3);
+    } else if (opc === 2) {
+        resultado = funcao_rampa(entrada1 * peso1 + entrada2 * peso2 + entrada3 * peso3);
+    } else if (opc === 3) {
+        resultado = funcao_sigmoide(entrada1 * peso1 + entrada2 * peso2 + entrada3 * peso3);
+    } else {
+        console.log("Escolha de função inválida.");
+        return;
+    }
+    
+    console.log("Saída calculada:", resultado);
+}
+
+main();
